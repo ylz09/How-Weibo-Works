8d5ce2329d69a6c20ab473434724715abc16ab91
macos <lizhou@udel.edu>
2018-10-15 09:17:10 -0400

    python reducer

--- map_reduce/reducer.py
+++ map_reduce/reducer.py
@@ -1,19 +1,4 @@
 #!/usr/bin/env python
-import sys
-# maps words to their counts
-word2count = {}
- 
-# input comes from STDIN (standard input)
-for line in sys.stdin:
-    # remove leading and trailing whitespace
-    line = line.strip()
-    # split the line into words while removing any empty strings
-    words = filter(lambda word: word, line.split())
-    # increase counters
-    for word in words:
-        print '%s\t%s' % (word, 1)
-#---------------------------------------------------------------------------------------------------------
-#!/usr/bin/env python
  
 from operator import itemgetter
 import sys
